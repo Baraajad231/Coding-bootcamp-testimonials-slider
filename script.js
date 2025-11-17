da0bc8446d2e6ad8bb46a5428f733660396af693
BARAA A. A. JADALLAH <brajadallh9@gmail.com>
2025-11-17 20:56:20 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,65 @@
+const slide = document.querySelectorAll(".slide");
+let currentIndex = 0;
+
+const updateSlider = () => {
+  const slider = document.querySelector(".slider");
+  slider.style.transform = `translateX(-${currentIndex * 100}vw)`;
+};
+
+const nextBtns = document.querySelectorAll(".next");
+nextBtns.forEach((btn) =>
+  btn.addEventListener("click", () => {
+    if (currentIndex < slide.length - 1) {
+      currentIndex++;
+      updateSlider();
+      bolitsHandler();
+    }
+  })
+);
+
+const backBtns = document.querySelectorAll(".back");
+backBtns.forEach((btn) =>
+  btn.addEventListener("click", () => {
+    if (currentIndex > 0) {
+      currentIndex--;
+      updateSlider();
+      bolitsHandler();
+    }
+  })
+);
+
+const createBolits = (num) => {
+  const slider = document.querySelector(".slider");
+  let bolitsHTML = `<li class="bolit bolit_active"></li>`;
+  for (let i = 1; i < num; i++) {
+    bolitsHTML += `<li class="bolit"></li>`;
+  }
+  const bolitsList = `<ul class="bolits">${bolitsHTML}</ul>`;
+  slider.insertAdjacentHTML("afterend", bolitsList);
+};
+
+let bolits;
+
+const bolitsHandler = () => {
+  const active = document.querySelector(".bolit_active");
+  if (active) active.classList.remove("bolit_active");
+  bolits[currentIndex].classList.add("bolit_active");
+};
+
+// عشان نفعل الbolit تاع اول صورة
+document.addEventListener("DOMContentLoaded", () => {
+  createBolits(slide.length);
+  bolits = document.querySelectorAll(".bolit");
+  bolitsHandler();
+});
+
+const btnsKeyboardHandler = () => {
+  document.addEventListener("keydown", (event) => {
+    if (event.key === "ArrowRight") {
+      nextBtns[currentIndex].click();
+    } else if (event.key === "ArrowLeft") {
+      backBtns[currentIndex].click();
+    }
+  });
+};
+btnsKeyboardHandler();
